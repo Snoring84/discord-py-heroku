7b8c97da71274bdb6a0ab45814432006d24a8eb4
Snoring84 <110753149+Snoring84@users.noreply.github.com>
2022-08-06 23:51:17 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,13 +4,13 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
 @bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
+async def on_message(message):
+	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	if message.content == "hello":
+		# SENDS BACK A MESSAGE TO THE CHANNEL.	
+		await message.channel.send("hey dirtbag")
+        
 if __name__ == "__main__":
     bot.run(TOKEN)
